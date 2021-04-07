a7ca19525afc54f11e85ba7333c6fd6e7cf5f3db
Truong <duytruong5437@gmail.com>
2021-04-07 07:08:15 -0700

    Them

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Styles'
 
 ReactDOM.render(
   <React.StrictMode>
